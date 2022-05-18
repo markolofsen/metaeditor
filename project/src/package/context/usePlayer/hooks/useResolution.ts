cffe8c007e927132388ab6b3255ae7d99e59a6bc
mark <vec@icareum.com>
2022-05-18 14:38:57 +0800

    fix

--- project/src/package/context/usePlayer/hooks/useResolution.ts
+++ project/src/package/context/usePlayer/hooks/useResolution.ts
@@ -33,9 +33,6 @@ export const useResolution = ({ mode = null }: any) => {
 		const objectFit = width > height ? 'unset' : 'fill'
 		doc.style.setProperty("--player-object-fit", objectFit);
 
-		const body = document.getElementsByTagName('body')[0]
-		body.style.setProperty('overscroll-behavior', 'contain')
-
 		// Update resolution of pixel-streaming
 		cls.updateWebrtcResolution(width, height)
 
