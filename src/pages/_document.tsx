1898bb319c10691bcb63761bd6b5490420ee4861
mark <vec@icareum.com>
2023-03-03 16:38:35 +0530

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,8 @@ import Document, {
 } from 'next/document';
 import createEmotionServer from '@emotion/server/create-instance';
 import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
-import theme, { roboto } from '../mui/theme';
-import createEmotionCache from '../mui/createEmotionCache';
+import theme, { roboto } from '../theme/theme';
+import createEmotionCache from '../theme/createEmotionCache';
 
 interface MyDocumentProps extends DocumentProps {
   emotionStyleTags: EmotionJSX.Element[];
